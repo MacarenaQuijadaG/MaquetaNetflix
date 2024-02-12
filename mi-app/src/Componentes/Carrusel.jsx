01d01278b0d795deeb17d7e37cbada63fcde83c1
mcarena <mcarena.quijada@outlook.com>
2024-02-12 01:50:51 -0300

    Se incorpora arreglos al carrusel como tambien la hoja de estilos

--- mi-app/src/Componentes/Carrusel.jsx
+++ mi-app/src/Componentes/Carrusel.jsx
@@ -1,31 +1,22 @@
 import Carousel from 'react-bootstrap/Carousel';
+import '../Css/Carrusel.css'
 
 function DarkVariantExample() {
   return (
     <Carousel data-bs-theme="dark">
       <Carousel.Item>
         <div className='container'>
-            <img className= 'd-block w-100' src={require('../imagenes/aqua.jpg' )} alt='' />
-            <img className= 'd-block w-100' src={require('../imagenes/coco.jpg' )} alt='' />
-            <img className= 'd-block w-100' src={require('../imagenes/et.jpg' )} alt='' />
-            <img className= 'd-block w-100' src={require('../imagenes/huye.jpg' )} alt='' />
+            <div className='rd-flex justify-content-center'>
+                <img className= 'img-fluid mx-2' src={require('../imagenes/aqua.jpg' )} alt='' />
+                <img className= 'img-fluid mx-3' src={require('../imagenes/aladin.jpg' )} alt='' />
+                <img className= 'img-fluid mx-3' src={require('../imagenes/et.jpg' )} alt='' />
+                <img className= 'img-fluid mx-3' src={require('../imagenes/troll.jpg' )} alt='' />
+                <img className= 'img-fluid mx-3' src={require('../imagenes/adams.jpg' )} alt='' />
+                <img className= 'img-fluid mx-3' src={require('../imagenes/black.jpg' )} alt='' />
+            </div>
         </div>
-      </Carousel.Item>
-      <Carousel.Item>
-        <div className='container'>
-            <img className= 'd-block w-100' src={require('../imagenes/aqua.jpg' )} alt='' />
-            <img className= 'd-block w-100' src={require('../imagenes/coco.jpg' )} alt='' />
-            <img className= 'd-block w-100' src={require('../imagenes/et.jpg' )} alt='' />
-            <img className= 'd-block w-100' src={require('../imagenes/huye.jpg' )} alt='' />
-        </div>
-      </Carousel.Item>
-      <Carousel.Item>
-      <div className='container'>
-            <img className= 'd-block w-100' src={require('../imagenes/aqua.jpg' )} alt='' />
-            <img className= 'd-block w-100' src={require('../imagenes/coco.jpg' )} alt='' />
-            <img className= 'd-block w-100' src={require('../imagenes/et.jpg' )} alt='' />
-            <img className= 'd-block w-100' src={require('../imagenes/huye.jpg' )} alt='' />
-     </div>
+      
+     
       </Carousel.Item>
     </Carousel>
   );
